4a8b349ac96f1b72a40ac00b4022596bb2a5dd38
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-25 16:27:12 -0700

    Error handling updates

--- ratings.py
+++ ratings.py
@@ -15,12 +15,20 @@ def list_restaurant_ratings(filename):
             restaurant, rating = line.rstrip().split(":")
             rest_ratings[restaurant] = rating
 
-        restaurant = input("Please enter a restaurant name.\n>").title()
-        rating = input("Please enter a rating for that restaurant.\n>")
-        rest_ratings[restaurant] = rating
+    restaurant = input("Please enter a restaurant name.\n>").title()
+    rating = 0
 
-        for restaurant, rating in sorted(rest_ratings.items()):
-            print(f'{restaurant} is rated at {rating}.')
+    while rating not in [1, 2, 3, 4, 5]:
+        try:
+            rating = int(input("Please enter a rating for that restaurant from 1 to 5.\n>"))
+            continue
+        except ValueError:
+            print("Not a valid entry!")
+
+    rest_ratings[restaurant] = rating
+
+    for restaurant, rating in sorted(rest_ratings.items()):
+        print(f'{restaurant} is rated at {rating}.')
 
 
 list_restaurant_ratings(sys.argv[1])
